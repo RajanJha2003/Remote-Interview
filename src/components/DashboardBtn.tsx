f028104d6cc1bb3245e01b954c792a4283adef84
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-22 17:55:12 +0530

    userRole hooks

--- src/components/DashboardBtn.tsx
+++ src/components/DashboardBtn.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
 
-const DashboardBtn = () => {
+import { useUserRole } from '@/hooks/useUserRole'
+import Link from 'next/link';
+import React from 'react'
+import { Button } from './ui/button';
+import { SparklesIcon } from 'lucide-react';
+
+const DashboardBtn = () =>{
+    const {isLoading,isCandidate}=useUserRole();
+
+    if(!isCandidate || isLoading) return null;
   return (
-    <div>DashboardBtn</div>
+    <Link href={"/dashboard"}>
+        <Button className='gap-2 font-medium' size={"sm"}>
+            <SparklesIcon className='size-4' />
+            Dashboard
+        </Button>
+
+    </Link>
+    
   )
 }
 
