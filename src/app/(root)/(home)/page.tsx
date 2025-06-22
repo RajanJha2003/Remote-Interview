a59f85ce7a7504b255e140eb1c2ffc48e7cfdf79
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-22 21:44:30 +0530

    Action Card Home Page

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import ActionCard from '@/components/ActionCard'
+import { Button } from '@/components/ui/button'
+import { QUICK_ACTIONS } from '@/constants'
+import { useUserRole } from '@/hooks/useUserRole'
+import { SignInButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+
+  const {isCandidate,isInterviewer,isLoading}=useUserRole();
+
+  return (
+    <div className='container max-w-7xl mx-auto p-6'>
+      <div className='rounded-lg bg-card p-6 border shadow-sm mb-10'>
+        <h1 className='text-4xl font-bold bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent'>Welcome back!</h1>
+      </div>
+      <p className='text-muted-foreground mt-2'>
+        {
+          isInterviewer ? "Manage your interviews and review candidates effectively":"Access your upcoming interviews and preparations"
+        }
+
+      </p>
+      {
+        !isInterviewer ? (
+          <>
+          <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+          {QUICK_ACTIONS.map((action) => (
+              <ActionCard
+                key={action.title}
+                action={action}
+                onClick={() => {}}
+              />
+            ))}
+
+          </div>
+          </>
+
+        ):(
+          <div>
+            xyz
+          </div>
+        )
+      }
+      
+
+    </div>
+    
+  )
+}
+
+export default page
